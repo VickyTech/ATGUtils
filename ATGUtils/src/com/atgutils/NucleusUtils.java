ad3e521420608d2f8dd473f8b28e1552f7f0635a
vigneswaran <vicky_sitaraman@yahoo.com>
2015-06-08 22:06:46 +0530

    Testing Egit

---
+++ ATGUtils/src/com/atgutils/NucleusUtils.java
@@ -0,0 +1,13 @@
+package com.atgutils;
+
+public class NucleusUtils {
+
+	@Override
+	public String toString() {
+		return "NucleusUtils [getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+	
+
+}
